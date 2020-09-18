7619133b22dffbd4f05db082127e2109e2f6b7dc
god-jiang <1440113361@qq.com>
2020-09-18 21:09:16 +0800

    leetcode14，最长公共前缀

---
+++ src/leetcode/LongestCommonPrefix.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * leetcode14，最长公共前缀
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串""。
+ * @author god-jiang
+ */
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String ans = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+            for (; j < ans.length() && j < strs[i].length(); j++) {
+                if (ans.charAt(j) != strs[i].charAt(j)) {
+                    break;
+                }
+            }
+            ans = ans.substring(0, j);
+            if (ans.equals("")) {
+                return ans;
+            }
+        }
+        return ans;
+    }
+}
