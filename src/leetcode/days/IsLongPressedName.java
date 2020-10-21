8a297d1fd1ac5be79599d2f639d80b8b96c354ea
god-jiang <1440113361@qq.com>
2020-10-21 21:26:15 +0800

    leetcode925，长按键入（双指针解法）

---
+++ src/leetcode/days/IsLongPressedName.java
@@ -0,0 +1,26 @@
+package leetcode.days;
+
+/**
+ * leetcode925，长按键入
+ * 你的朋友正在使用键盘输入他的名字。偶尔，在输入字符c时，按键可能会被长按，而字符可能被输入1次或多次。
+ * 你将会检查键盘输入的字符typed。如果它对应的可能是你的朋友的名字（其中一些字符可能被长按），那么返回true.
+ *
+ * @author god-jiang
+ */
+public class IsLongPressedName {
+    public boolean isLongPressedName(String name, String typed) {
+        int i = 0;
+        int j = 0;
+        while (j < typed.length()) {
+            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
+                i++;
+                j++;
+            } else if (j > 0 && typed.charAt(j) == typed.charAt(j - 1)) {
+                j++;
+            } else {
+                return false;
+            }
+        }
+        return i == name.length();
+    }
+}
