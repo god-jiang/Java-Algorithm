94dd22fea743ddd3e8c08f5ce1b08b61970e3b41
god-jiang <1440113361@qq.com>
2020-10-29 20:45:40 +0800

    leetcode129，求根到叶子节点数字之和（dfs）

---
+++ src/leetcode/days/SumNumbers.java
@@ -0,0 +1,31 @@
+package leetcode.days;
+
+import leetcode.struct.TreeNode;
+
+/**
+ * leetcode129. 求根到叶子节点数字之和
+ * 给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+ * <p>
+ * 例如，从根到叶子节点路径 1->2->3 代表数字 123。
+ * <p>
+ * 计算从根到叶子节点生成的所有数字之和。
+ * <p>
+ * 说明: 叶子节点是指没有子节点的节点。
+ */
+public class SumNumbers {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode root, int pSum) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = pSum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        } else {
+            return dfs(root.left, sum) + dfs(root.right, sum);
+        }
+    }
+}
