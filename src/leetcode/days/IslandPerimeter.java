0bdc159eeafb964f5d2146b9e63a8df818a2c4b9
god-jiang <1440113361@qq.com>
2020-11-01 21:16:27 +0800

    leetcode463，岛屿的周长（dfs）

---
+++ src/leetcode/days/IslandPerimeter.java
@@ -0,0 +1,37 @@
+package leetcode.days;
+
+/**
+ * 463. 岛屿的周长
+ * 给定一个包含 0 和 1 的二维网格地图，其中 1 表示陆地 0 表示水域。
+ * <p>
+ * 网格中的格子水平和垂直方向相连（对角线方向不相连）。整个网格被水完全包围，但其中恰好有一个岛屿（或者说，一个或多个表示陆地的格子相连组成的岛屿）。
+ * <p>
+ * 岛屿中没有“湖”（“湖” 指水域在岛屿内部且不和岛屿周围的水相连）。格子是边长为 1 的正方形。网格为长方形，且宽度和高度均不超过 100 。计算这个岛屿的周长。
+ *
+ * @author god-jiang
+ */
+public class IslandPerimeter {
+    public int islandPerimeter(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    return dfs(grid, i, j);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public int dfs(int[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] == 0) {
+            return 1;
+        }
+        if (grid[x][y] != 1) {
+            return 0;
+        }
+        grid[x][y] = 2;
+        return dfs(grid, x + 1, y) + dfs(grid, x, y + 1) + dfs(grid, x - 1, y) + dfs(grid, x, y - 1);
+    }
+}
