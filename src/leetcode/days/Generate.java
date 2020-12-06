b76bf0dffa60b0f862ebc6c03d91e6d91f9ee74e
god-jiang <1440113361@qq.com>
2020-12-06 20:06:09 +0800

    118. 杨辉三角

---
+++ src/leetcode/days/Generate.java
@@ -0,0 +1,28 @@
+package leetcode.days;
+
+import java.util.*;
+
+/**
+ * 118. 杨辉三角
+ *
+ * @author god-jiang
+ * @date 2020/12/6 20:05
+ */
+public class Generate {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    list.add(1);
+                } else {
+                    list.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+                }
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
